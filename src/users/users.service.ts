3704f36c15e1612bf3423d7dd7bbe404a3262b57
unknown <Melissa Miranda>
2022-12-12 11:30:54 -0300

    segunda tentativa

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,11 +1,11 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
-import {User} from '.entities/user.entity'
+import { User } from './entities/user.entity';
 
 @Injectable()
 export class UsersService {
-  private user: User[]=[];
+  private users: User[]=[];
   
   create(createUserDto: CreateUserDto) {
     const currentMaxId = this.users[this.users.length - 1]?.id || 0;
@@ -22,18 +22,39 @@ export class UsersService {
   }
 
   findAll() {
-    return `This action returns all users`;
+    return this.users;
   }
 
   findOne(id: number) {
-    return `This action returns a #${id} user`;
+    const index = this.users.findIndex((user) => user.id === id);
+
+    return this.users[index];
   }
 
   update(id: number, updateUserDto: UpdateUserDto) {
-    return `This action updates a #${id} user`;
+    const user = this.findOne(id);
+
+    const newUser = {
+      ...user,
+      ...updateUserDto,
+    };
+
+    const index = this.users.findIndex((user) => user.id === id);
+
+    this.users[index] = newUser;
+    
+    return newUser;
   }
 
   remove(id: number) {
+    const index = this.users.findIndex((user) => user.id === id);
+
+    if(index === -1) {
+      throw new NotFoundException('User with this id does not exists');
+    }
+
+    this.users.splice(index, 1);
+
     return `This action removes a #${id} user`;
   }
 }
